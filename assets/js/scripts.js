441e6588b38caee04de680c96861e21430c6a4a5
Stanislav Kushnir <must3r@gmail.com>
2016-12-13 17:07:43 +0200

    break a leg :)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-// announcing the variables
+    // announcing the variables
     var picImg = $('.wcalc__frame .pic'),
         picImgRev = $('.wcalc__frame .pic-rev'),
         picImgSec = $('.wcalc__frame .pic-sec'),
@@ -8,14 +8,14 @@ $(document).ready(function() {
         allClasses = 'active effect-1 effect-2 effect-1-reverse effect-2-reverse effect-3';
 
     $('.wcalc__icons .gal-icon').on('click', function() {
-      var panelToShow = $(this).attr('rel');
+        var panelToShow = $(this).attr('rel');
 
-      $('.gal-icon.checked').removeClass('checked');
-      $(this).addClass('checked');
+        $('.gal-icon.checked-icon').removeClass('checked-icon');
+        $(this).addClass('checked-icon');
 
-      $('.wcalc__box.isframe').removeClass('isframe');
-      $('.'+panelToShow).addClass('isframe');
-      $('.wcalc__frame img').removeClass(allClasses);
+        $('.wcalc__box.isframe').removeClass('isframe');
+        $('.' + panelToShow).addClass('isframe');
+        $('.wcalc__frame img').removeClass(allClasses);
     })
 
     trigger.on('click', function() {
@@ -61,6 +61,89 @@ $(document).ready(function() {
     $('.turn-down-third').on('click', function() {
         picImgThird.removeClass(allClasses);
         picImgThird.addClass('active effect-3');
+    });
+
+    var sliderVone = document.getElementById('slider-v-one');
+
+    noUiSlider.create(sliderVone, {
+        start: [1650],
+        orientation: "vertical",
+        step: 10,
+        range: {
+            'min': [1000],
+            'max': [1650]
+        },
+        pips: {
+            mode: 'values',
+            values: [1000, 1650],
+            density: 2
+        }
+    });
+
+    var slider2 = document.getElementById('slider2');
+
+    noUiSlider.create(slider2, {
+        start: [1000],
+        step: 10,
+        range: {
+            'min': [1000],
+            'max': [1400]
+        },
+        pips: {
+            mode: 'values',
+            values: [1000, 1400],
+            density: 2
+        }
+    });
+
+    var sliderVtwo = document.getElementById('slider-v-two');
+
+    noUiSlider.create(sliderVtwo, {
+        start: [1500],
+        orientation: "vertical",
+        step: 10,
+        range: {
+            'min': [1500],
+            'max': [2200]
+        },
+        pips: {
+            mode: 'values',
+            values: [1500, 2200],
+            density: 2
+        }
+    });
+
+    var slider4 = document.getElementById('slider4');
+
+    noUiSlider.create(slider4, {
+        start: [1000],
+        step: 10,
+        range: {
+            'min': [1000],
+            'max': [1700]
+        },
+        pips: {
+            mode: 'values',
+            values: [1000, 1700],
+            density: 2
+        }
+    });
+
+    var sliderVthree = document.getElementById('slider-v-three');
+
+    noUiSlider.create(sliderVthree, {
+        start: [950],
+        orientation: "vertical",
+        step: 10,
+        range: {
+            'min': [950],
+            'max': [2200]
+        },
+        pips: {
+            mode: 'values',
+            values: [900, 2200],
+            density: 2
+        }
     });
 
 });
